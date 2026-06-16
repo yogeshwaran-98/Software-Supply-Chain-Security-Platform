d2daf7016da3fa871e082a6bccbc0d8280d81fd0
yogeshwaran-98 <mryogeshwaran98@gmail.com>
2026-06-16 14:44:58 +0530

    dc

---
+++ app/tests/test_app.py
@@ -0,0 +1,22 @@
+import pytest
+from app.app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_endpoint(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["message"] == "Zero Trust Supply Chain Platform"
+
+
+def test_health_endpoint(client):
+    response = client.get("/health")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["status"] == "healthy"
