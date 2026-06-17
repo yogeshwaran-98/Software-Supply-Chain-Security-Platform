2bb7dd696f23400db708dcc7139298c1ea1a127a
yogeshwaran-98 <mryogeshwaran98@gmail.com>
2026-06-17 07:24:00 +0530

    checkov

--- scripts/checkov_gate.py
+++ scripts/checkov_gate.py
@@ -6,16 +6,17 @@ with open("reports/checkov.json") as f:
 
 fail = False
 
-for r in data["results"]["failed_checks"]:
-    severity = r.get("severity", "UNKNOWN").upper()
+for report in data:
+    for r in report["results"]["failed_checks"]:
+        severity = r.get("severity", "UNKNOWN").upper()
 
-    if severity in ["CRITICAL", "HIGH"]:
-        print(f"❌ Blocking issue: {r['check_id']} - {severity}")
-        fail = True
-    elif severity == "MEDIUM":
-        print(f"⚠️ Warning: {r['check_id']} - {severity}")
-    else:
-        print(f"ℹ️ Info: {r['check_id']} - {severity}")
+        if severity in ["CRITICAL", "HIGH"]:
+            print(f"❌ Blocking issue: {r['check_id']} - {severity}")
+            fail = True
+        elif severity == "MEDIUM":
+            print(f"⚠️ Warning: {r['check_id']} - {severity}")
+        else:
+            print(f"ℹ️ Info: {r['check_id']} - {severity}")
 
 if fail:
     sys.exit(1)
